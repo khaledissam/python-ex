c343b7abd3c736d2671f7ca8a2e1987197678de6
Minjie Zhu <zhum@onid.oregonstate.edu>
2015-07-10 11:20:05 -0700

    ex2-4

--- ex1.py
+++ ex1.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
+
+# ex1
 print "Hello World!"
 print "Hello Again"
 print "This is good"
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print "你好世界"
+
+# ex2 and ex3
+print "I will now count my chickens:"
+print "Hens", 25+30/6
+print "Is it true that 3+2<5-7?"
+print 3+2<5-7
+
+# ex4: variables and names
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+car_not = cars-drivers
+car_dri = drivers
+carpool = car_dri*space_in_a_car
+ave = passengers/car_dri
+
+print "There are ", cars, "cars available."
+print "There are only ", drivers, "drivers available."
+print "There will be ", car_not, "empty cars today."
+print "We can transport ", carpool, "people today."
+print "We have ", passengers, "to carpool today."
+print "We need to put about ", ave, "in each car."
+
