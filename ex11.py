8a51d2480dcf7ceda4b2f6eb4ffd76fc9d2992c1
Minjie Zhu <zhum@onid.oregonstate.edu>
2015-07-16 17:08:24 -0700

    ex12-15

--- ex11.py
+++ ex11.py
@@ -9,3 +9,9 @@ print "How much do you weigh?",
 weight = raw_input()
 
 print "So, you'are %r old, %r tall and %r heavy."%(age,height,weight)
+
+print "Ex-12"
+age = raw_input("How old? ")
+height = raw_input("How tall? ")
+weight = raw_input("How heavy? ")
+print "So, you'are %r old, %r tall and %r heavy."%(age,height,weight)
